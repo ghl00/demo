0597b952de1174cf326f84bbab938763a4032412
guliu <Guanghao.Liu@rbbn.com>
2024-04-15 19:04:33 +0800

    audio

---
+++ AudioMixer.h
@@ -0,0 +1,93 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <array>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <chrono>
+#include <utility>
+#include <algorithm>
+#include <sstream>
+#include <new>
+#include <memory>
+
+class SDLAudioSource
+{
+private:
+    bool _released{};
+public:
+    virtual ~SDLAudioSource();
+    bool IsReleased() const;
+    virtual uint64_t GetDataLength() const = 0;
+};
+
+class FlacAudioSource final : public SDLAudioSource
+{
+private:
+    uint64_t _dataLength;
+public:
+    FlacAudioSource(uint64_t dataLength) : _dataLength(dataLength) {};
+    uint64_t GetDataLength() const override
+    {
+        return _dataLength;
+    }
+};
+
+
+
+struct ISDLAudioChannel
+{
+    virtual ~ISDLAudioChannel();
+
+    virtual int GetLoop() const = 0;
+    virtual void SetLoop(int loop) = 0;
+};
+
+template<typename AudioSource_ = SDLAudioSource>
+class AudioChannelImpl final : public ISDLAudioChannel
+{
+private:
+    int _loop;
+
+public:
+    virtual ~AudioChannelImpl() = default;
+    AudioChannelImpl(int loop) : _loop(loop) {};
+
+    int GetLoop() const override
+    {
+        return _loop;
+    }
+
+    void SetLoop(int loop) override
+    {
+        _loop = loop;
+    }
+};
+
+class AudioMixer
+{
+private:
+    std::vector<std::unique_ptr<SDLAudioSource>> _sources;
+    std::list<std::shared_ptr<ISDLAudioChannel>> _channels;
+
+public:
+    virtual ~AudioMixer();
+    void Play();
+    void GetNextAudioChunk();
+    ISDLAudioChannel* GetChannel();
+    SDLAudioSource* AddSource(std::unique_ptr<SDLAudioSource> source);
+    void RemoveReleasedSources();
+    void Dump();
+    void DumpChannelUserCount();
+    void DumpChannelUserCount(std::shared_ptr<ISDLAudioChannel>& channel);
+
+};
+
+namespace AudioChannel
+{
+    ISDLAudioChannel* Create(int loop);
+}
+
+std::unique_ptr<SDLAudioSource> CreateAudioSource(uint64_t dataLength);
